a1ab9ee8160d26cbf6df8195f5bbb238d58cb7bc
Farai Chawora <malonechawora@gmail.com>
2021-04-01 12:23:48 +0200

    commit #2

---
+++ src/main/java/ac/za/cput/appdev/TestingApp.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac.za.cput.appdev;
+
+/**
+ *
+ * team member name : Farai Malone Chawora
+ *Student number: 220145547
+ */
+public class TestingApp {
+    public int multiply(int num,int num2){
+    int result=num*num2;
+    return result;
+    }
+   /* public  int addition(String s1,String s2){
+    return 0;
+    }*/
+}
