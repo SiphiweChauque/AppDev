98900fcff2db537e6d5b91be3fc48c7b561add02
Farai Chawora <malonechawora@gmail.com>
2021-04-01 18:01:53 +0200

    final commit

--- src/main/java/ac/za/cput/appdev/App.java
+++ src/main/java/ac/za/cput/appdev/App.java
@@ -8,11 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        TestingApp multiplication=new TestingApp ();
-        multiplication.multiply(6564, 848);
-        System.out.println("the multiple of 6564 and 848 is "+multiplication);
+        TestingApp math=new TestingApp ();
+        math.multiply(6564, 848);
         
-        
+        System.out.println("the multiple of 6564 and 848 is "+math);
+      
     }
     
 }
