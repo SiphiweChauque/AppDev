be601d62a87b10cd141aaee271d895436f226f6d
Cristina <220344663@mycput.ac.za>
2021-04-01 15:15:33 +0100

    Second Commit

---
+++ src/main/java/ac/za/cput/appdev/Hockey.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ac.za.cput.appdev;
+
+/**
+ *
+ * @author Zela NNeizla Inacio Buta
+ */
+public class Hockey {
+    private String naame;
+    private int gameWon;
+    private int gameLost;
+    private int Scores;
+
+    public String getNaame() {
+        return naame;
+    }
+
+    public void setNaame(String naame) {
+        this.naame = naame;
+    }
+
+    public int getGameWon() {
+        return gameWon;
+    }
+
+    public void setGameWon(int gameWon) {
+        this.gameWon = gameWon;
+    }
+
+    public int getGameLost() {
+        return gameLost;
+    }
+
+    public void setGameLost(int gameLost) {
+        this.gameLost = gameLost;
+    }
+
+    public int getScores() {
+        return Scores;
+    }
+
+    public void setScores(int Scores) {
+        this.Scores = Scores;
+    }
+    
+    
+}
